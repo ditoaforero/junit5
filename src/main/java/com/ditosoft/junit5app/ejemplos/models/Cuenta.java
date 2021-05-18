09ec95db12e1bcdaa7f724776f56565171754122
Eduardo Arevalo <eforero@stratio.com>
2021-05-18 18:57:19 -0500

    Se incorporan validaciones de excepciones

--- src/main/java/com/ditosoft/junit5app/ejemplos/models/Cuenta.java
+++ src/main/java/com/ditosoft/junit5app/ejemplos/models/Cuenta.java
@@ -1,5 +1,7 @@
 package com.ditosoft.junit5app.ejemplos.models;
 
+import com.ditosoft.junit5app.ejemplos.exceptions.DineroInsuficienteException;
+
 import java.math.BigDecimal;
 import java.util.Locale;
 
@@ -10,7 +12,8 @@ public class Cuenta {
 
     public Cuenta(String persona, BigDecimal saldo) {
         this.saldo = saldo;
-        this.persona = persona.toUpperCase();
+        //this.persona = persona.toUpperCase();
+        this.persona = persona;
     }
 
     public String getPersona() {
@@ -27,5 +30,30 @@ public class Cuenta {
 
     public void setSaldo(BigDecimal saldo) {
         this.saldo = saldo;
+    }
+
+    public void debito(BigDecimal monto){
+        BigDecimal nuevoSaldo = this.saldo.subtract(monto);
+        if (nuevoSaldo.compareTo(BigDecimal.ZERO) < 0)
+            throw new DineroInsuficienteException("Dinero insuficiente");
+        this.saldo = nuevoSaldo;
+    }
+
+    public void credito(BigDecimal monto){
+        this.saldo = this.saldo.add(monto);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if(obj == null || !(obj instanceof Cuenta))
+            return false;
+
+        Cuenta cuenta = (Cuenta)obj;
+
+        if(this.persona == null || this.saldo== null)
+            return false;
+
+        return this.persona.equals(cuenta.getPersona()) && this.saldo.equals(cuenta.getSaldo());
     }
 }
--- src/test/java/com/ditosoft/junit5app/ejemplos/models/CuentaTest.java
+++ src/test/java/com/ditosoft/junit5app/ejemplos/models/CuentaTest.java
@@ -1,6 +1,7 @@
 package com.ditosoft.junit5app.ejemplos.models;
 
 
+import com.ditosoft.junit5app.ejemplos.exceptions.DineroInsuficienteException;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -14,6 +15,7 @@ class CuentaTest {
         Cuenta cuenta = new Cuenta("Eduardo",new BigDecimal("1000.12345"));
         String esperado = "Eduardo";
         String real = cuenta.getPersona();
+        assertNotNull(real);
         assertEquals(esperado,real);
         assertTrue(real.equals("Eduardo"));
     }
@@ -21,7 +23,48 @@ class CuentaTest {
     @Test
     void testSaldoCuenta(){
         Cuenta cuenta = new Cuenta("Eduardo", new BigDecimal("10000.12345"));
+        assertNotNull(cuenta.getSaldo());
         assertEquals(10000.12345,cuenta.getSaldo().doubleValue());
         assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO)<0);
+    }
+
+    @Test
+    void testReferenciaCuenta() {
+        Cuenta cuenta = new Cuenta("Jhon Doe", new BigDecimal("8900.99997"));
+        Cuenta cuenta2 = new Cuenta("Jhon Doe", new BigDecimal("8900.99997"));
+        //assertNotEquals(cuenta2,  cuenta);
+        assertEquals(cuenta2,  cuenta);
+    }
+
+    @Test
+    void testDebitoCuenta() {
+        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
+        cuenta.debito(new BigDecimal("100"));
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(900, cuenta.getSaldo().intValue());
+        assertEquals("900.12345", cuenta.getSaldo().toPlainString());
+
+    }
+
+    @Test
+    void testCreditoCuenta() {
+        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
+        cuenta.credito(new BigDecimal("100"));
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(1100, cuenta.getSaldo().intValue());
+        assertEquals("1100.12345", cuenta.getSaldo().toPlainString());
+
+    }
+
+    @Test
+    void testDineroInsuficienteExceptionCuenta() {
+        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
+        Exception exception = assertThrows(DineroInsuficienteException.class, ()-> {
+           cuenta.debito(new BigDecimal(1500));
+        });
+        String actual =exception.getMessage();
+        String esperado = "Dinero insuficiente";
+        assertEquals(esperado, actual);
+
     }
 }
